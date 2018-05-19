f23e00a4ad7663a62494dd3fa3e12a87dfa3a073
Goksel Ozardali <gokselozardali@gmail.com>
2018-05-19 03:07:39 +0300

    unityCountdown
    Countdown with unity

---
+++ Assets/countdown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdown: MonoBehaviour {
+
+	public Text countdownText;
+	public int second = 00,
+	minute = 10;
+
+	void Start() {
+		StartCoroutine("Countdown");
+	}
+
+	void Update() {
+		if (minute > 00) {
+			if (second == 00) {
+				second = 59;
+				minute--;
+			}
+		}
+		if ((minute == 00) && (second == 00)) {
+			StopCoroutine("Countdown");
+			//Debug.Log("Time is up");
+		}
+	}
+	IEnumerator Countdown() {
+		while (true) {
+			yield
+			return new WaitForSeconds(1);
+			second--;
+			countdownText.text = "" + minute.ToString("D2") + ":" + second.ToString("D2");
+		}
+
+	}
+}
